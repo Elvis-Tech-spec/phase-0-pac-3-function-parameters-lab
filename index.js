721f37a993f04379583d3aab565ace80e75e591a
Elvis-Tech-spec <manfuturekahande@gmail.com>
2024-11-23 18:49:24 +0300

    modified:   index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("Aki"));  // Output: Hi, my name is Aki.
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguage("Aki", "Ember.js"));  
+// Output: Hi, my name is Aki and I am learning to program in Ember.js.
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguageOptional("Aki", "Ember.js"));  
+// Output: Hi, my name is Aki and I am learning to program in Ember.js.
+
+console.log(introductionWithLanguageOptional("Aki"));  
+// Output: Hi, my name is Aki and I am learning to program in JavaScript.
+
